13eee1f5ec2cba56f1d94e62c48c70ec117c3c99
Anurag Affection <anuragaffection07@gmail.com>
2023-12-24 22:51:47 +0530

    updated

--- src/components/PrivacyPolicy.jsx
+++ src/components/PrivacyPolicy.jsx
@@ -3,28 +3,41 @@ import { privacy } from '../constant/privacy';
 
 const PrivacyPolicy = () => {
   const container = `bg-gray-900 text-gray-200 p-4 `;
-  const subHeading = `text-2xl font-semibold mb-4 text-yellow-400`;
+  const wrapper = `bg-gray-950 py-4 px-6 m-1 rounded-md`
+  const subHeading = `text-2xl mb-4 text-yellow-400`;
   const paragraphStyle = `mb-4`;
 
   return (
     <div className={container}>
-      <h2 className={subHeading}>Privacy Policy</h2>
-      <p className={paragraphStyle}>{privacy.privacyPolicy}</p>
+      <div className={wrapper}>
+        <h2 className={subHeading}>Privacy Policy</h2>
+        <p className={paragraphStyle}>{privacy.privacyPolicy}</p>
+      </div>
 
-      <h3 className={subHeading}>Data Collection</h3>
-      <p className={paragraphStyle}>{privacy.dataCollection}</p>
+      <div className={wrapper}>
+        <h3 className={subHeading}>Data Collection</h3>
+        <p className={paragraphStyle}>{privacy.dataCollection}</p>
+      </div>
 
-      <h3 className={subHeading}>Data Usage</h3>
-      <p className={paragraphStyle}>{privacy.dataUsage}</p>
+      <div className={wrapper}>
+        <h3 className={subHeading}>Data Usage</h3>
+        <p className={paragraphStyle}>{privacy.dataUsage}</p>
+      </div>
 
-      <h3 className={subHeading}>Data Security</h3>
-      <p className={paragraphStyle}>{privacy.dataSecurity}</p>
+      <div className={wrapper}>
+        <h3 className={subHeading}>Data Security</h3>
+        <p className={paragraphStyle}>{privacy.dataSecurity}</p>
+      </div>
 
-      <h3 className={subHeading}>Terms & Conditions</h3>
-      <p className={paragraphStyle}>{privacy.termsAndConditons}</p>
+      <div className={wrapper}>
+        <h3 className={subHeading}>Terms & Conditions</h3>
+        <p className={paragraphStyle}>{privacy.termsAndConditons}</p>
+      </div>
 
-      <h3 className={subHeading}>Changes to Privacy Policy</h3>
-      <p className={paragraphStyle}>{privacy.changesToPrivacyPolicy}</p>
+      <div className={wrapper}>
+        <h3 className={subHeading}>Changes to Privacy Policy</h3>
+        <p className={paragraphStyle}>{privacy.changesToPrivacyPolicy}</p>
+      </div>
     </div>
   );
 };
