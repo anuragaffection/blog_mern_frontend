e26c1a193249cd9bb32d21397e0d3129a3c33f77
Anurag Affection <anuragaffection07@gmail.com>
2023-12-24 22:29:59 +0530

    Privacy Policy added

--- src/components/PrivacyPolicy.jsx
+++ src/components/PrivacyPolicy.jsx
@@ -2,27 +2,29 @@ import React from 'react';
 import { privacy } from '../constant/privacy';
 
 const PrivacyPolicy = () => {
+  const container = `bg-gray-900 text-gray-200 p-4 `;
+  const subHeading = `text-2xl font-semibold mb-4 text-yellow-400`;
+  const paragraphStyle = `mb-4`;
+
   return (
-    <div className="bg-gray-900 text-white p-6">
+    <div className={container}>
+      <h2 className={subHeading}>Privacy Policy</h2>
+      <p className={paragraphStyle}>{privacy.privacyPolicy}</p>
 
-      <h2 className="text-2xl font-semibold mb-4">Privacy Policy</h2>
-      <p className="mb-4">{privacy.privacyPolicy}</p>
+      <h3 className={subHeading}>Data Collection</h3>
+      <p className={paragraphStyle}>{privacy.dataCollection}</p>
 
-      <h3 className="text-xl font-semibold mb-2">Data Collection</h3>
-      <p className="mb-4">{privacy.dataCollection}</p>
+      <h3 className={subHeading}>Data Usage</h3>
+      <p className={paragraphStyle}>{privacy.dataUsage}</p>
 
-      <h3 className="text-xl font-semibold mb-2">Data Usage</h3>
-      <p className="mb-4">{privacy.dataUsage}</p>
+      <h3 className={subHeading}>Data Security</h3>
+      <p className={paragraphStyle}>{privacy.dataSecurity}</p>
 
-      <h3 className="text-xl font-semibold mb-2">Data Security</h3>
-      <p className="mb-4">{privacy.dataSecurity}</p>
+      <h3 className={subHeading}>Terms & Conditions</h3>
+      <p className={paragraphStyle}>{privacy.termsAndConditons}</p>
 
-      <h3 className="text-xl font-semibold mb-2">Terms & Conditions</h3>
-      <p className="mb-4">{privacy.termsAndConditons}</p>
-
-      <h3 className="text-xl font-semibold mb-2">Changes to Privacy Policy</h3>
-      <p className="mb-4">{privacy.changesToPrivacyPolicy}</p>
-
+      <h3 className={subHeading}>Changes to Privacy Policy</h3>
+      <p className={paragraphStyle}>{privacy.changesToPrivacyPolicy}</p>
     </div>
   );
 };
